dae4198218c7ca278c430c967ca491eaf906ae3b
Nikhil Chetty <nikhilchetty64@gmail.com>
2021-05-18 00:55:38 +0200

    Add project files.

---
+++ VisionApiDemo/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using VisionApiDemo.Models;
+using VisionApiDemo.Business_Layer;
+
+namespace VisionApiDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        VisionApiService obj = new VisionApiService();
+
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            var response = await obj.MakeAnalysisRequest();
+
+            return View(response);
+        }
+    }
+}
